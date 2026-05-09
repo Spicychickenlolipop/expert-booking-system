e5d09c90447171b9d46d158deb52f12d42056a1a
Anirudra Kisku <kisku.anirudra@gmail.com>
2026-05-10 02:43:52 +0530

    Updated production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://expert-booking-system-qkml.onrender.com/api",
 });
 
 export default API;
